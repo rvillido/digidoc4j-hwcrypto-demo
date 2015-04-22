f9319872f2696d0176e04652a6066ee672ac3f53
allan.juhanson <allan.juhanson@nortal.com>
2015-04-23 00:20:03 +0300

    IB-3923 initial commit with spring boot and view based on sign.html

---
+++ src/main/java/ee/sk/hwcrypto/demo/DssHwcryptoWebappApplication.java
@@ -0,0 +1,12 @@
+package ee.sk.hwcrypto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssHwcryptoWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DssHwcryptoWebappApplication.class, args);
+    }
+}
